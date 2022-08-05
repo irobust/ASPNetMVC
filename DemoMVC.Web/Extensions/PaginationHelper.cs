74c74ac41e014b7dd97fb729cdfbf4748e156012
Phanupong Permpimol <phanupong@irobust.co.th>
2022-08-05 17:02:45 +0700

    update pagination helper

--- DemoMVC.Web/Extensions/PaginationHelper.cs
+++ DemoMVC.Web/Extensions/PaginationHelper.cs
@@ -8,23 +8,23 @@ namespace DemoMVC.Web.Extensions
     {
         public static string PageLinks(this IHtmlHelper helper, IViewModel viewModel, Func<int, string> pageUrl)
         {
-            //StringBuilder result = new StringBuilder();
-            //for (int i = 1; i < viewModel.PagingInfo.TotalItems; i++)
-            //{
-            //    TagBuilder tag = new TagBuilder("a");
-            //    tag.MergeAttribute("href", pageUrl(i));
-            //    tag.InnerHtml = i.ToString();
-            //    if(i == viewModel.PagingInfo.CurrentPage)
-            //    {
-            //        tag.AddCssClass("Selected");
-            //        tag.AddCssClass("btn-primary");
-            //    }
-            //    tag.AddCssClass("btn btn-default");
-            //    result.Append(tag.ToString());
-            //}
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i < viewModel.PagingInfo.TotalItems; i++)
+            {
+                TagBuilder a = new TagBuilder("a");
+                a.MergeAttribute("href", pageUrl(i));
+                a.InnerHtml.AppendHtml(i.ToString());
+                if (i == viewModel.PagingInfo.CurrentPage)
+                {
+                    a.AddCssClass("Selected");
+                    a.AddCssClass("btn-primary");
+                }
+                a.AddCssClass("btn btn-default");
+                result.Append(a.ToString());
+            }
 
-            return $"current page is {viewModel.PagingInfo.CurrentPage}";
-            //return result;
+            //return $"current page is {viewModel.PagingInfo.CurrentPage}";
+            return result.ToString();
 
         }
     }
