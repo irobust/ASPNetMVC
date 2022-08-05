f829add218a6b2854f1cf10565c0e4a345278ca2
Phanupong Permpimol <phanupong@irobust.co.th>
2022-08-05 16:42:05 +0700

    add web api project

---
+++ DemoMVC.Web/Controllers/ValueController.cs
@@ -0,0 +1,15 @@
+﻿using DemoMVC.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoMVC.Web.Controllers
+{
+    public class ValueController : Controller
+    {
+        private Value value = new Value();
+
+        public string Index()
+        {
+            return value.GetValue()?.ToString() ?? "";
+        }
+    }
+}
---
+++ DemoMVC.Web/Models/Value.cs
@@ -0,0 +1,27 @@
+﻿namespace DemoMVC.Web.Models
+{
+    public class Value
+    {
+        private HttpClient client = new HttpClient();
+        public Value()
+        {
+            client.BaseAddress = new Uri("http://localhost:5211/api/");
+            client.DefaultRequestHeaders.Accept
+                .Add(new System.Net.Http.Headers
+                    .MediaTypeWithQualityHeaderValue("application/json"));
+            
+        }
+
+        public object? GetValue()
+        {
+            HttpResponseMessage response = client.GetAsync("Values/1").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadFromJsonAsync<object>().Result;
+
+            }
+            return new { errorCode = response.StatusCode };
+        }
+
+    }
+}
